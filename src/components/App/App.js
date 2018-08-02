9f1210cb9d3de3d12b3f0036822fe13f96286511
Malik Keenen Ahmed <kansomalik@gmail.com>
2018-08-02 10:50:49 -0400

    was able to set state properly after moving functions and lifecycle to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,11 +12,36 @@ import Results from "../Results/Results.js";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      summonerDetails: null
+      name: "",
+      summonerLevel: 0,
+      accountId: 0
     };
+  }
+  handleSearchInput(e) {
+    var summonerInput = e.target.value;
+  }
+  handleSearchSubmit(e) {}
+
+  componentDidMount() {
+    var api_key = "RGAPI-c2803147-5d63-4739-a85a-9181c1a7a66a";
+    axios
+      .get(
+        `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Senpai%20My%20Guy?api_key=${api_key}`
+      )
+      .then(response => {
+        console.log(response.data.name);
+        console.log(response.data.accountId);
+        console.log(response.data.summonerLevel);
+
+        this.setState({
+          name: response.data.name,
+          summonerLevel: response.data.summonerLevel,
+          accountId: response.data.accountId
+        });
+      });
   }
 
   render() {
@@ -30,13 +55,7 @@ class App extends Component {
           </nav>
           <main>
             <Switch>
-              <Route
-                path="/search"
-                component={Search}
-                // render={() => {
-                //   return <Redirect to="/search" />;
-                // }}
-              />
+              <Route path="/search" component={Search} />
             </Switch>
           </main>
         </div>
