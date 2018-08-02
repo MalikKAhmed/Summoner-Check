b6fa1950a049b5c04536875b045c110f5b3dc3de
Malik Keenen Ahmed <kansomalik@gmail.com>
2018-08-02 15:09:29 -0400

    trying to figure out how to get the set state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Redirect,
-  Switch
-} from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import axios from "axios";
 import Search from "../Search/Search.js";
-import Results from "../Results/Results.js";
+// import Results from "../Results/Results.js";
 import "./App.css";
 
 class App extends Component {
@@ -16,31 +10,52 @@ class App extends Component {
     super(props);
     this.state = {
       name: "",
-      summonerLevel: 0,
-      accountId: 0
+      summonerLevel: "",
+      accountId: ""
     };
   }
+
   handleSearchInput(e) {
-    var summonerInput = e.target.value;
+    // var summonerInput = e.target.value;
   }
   handleSearchSubmit(e) {}
 
   componentDidMount() {
     var api_key = "RGAPI-c2803147-5d63-4739-a85a-9181c1a7a66a";
+    var accountId = this.state.accountId;
     axios
       .get(
         `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Senpai%20My%20Guy?api_key=${api_key}`
       )
       .then(response => {
-        console.log(response.data.name);
-        console.log(response.data.accountId);
-        console.log(response.data.summonerLevel);
+        // console.log(response.data);
+        // console.log(response.data.name);
+        // console.log(response.data.accountId);
+        // console.log(response.data.summonerLevel);
+        // var { name, summonerLevel, accountId } = response.data;
+        this.setState(
+          {
+            name: response.data.name,
+            summonerLevel: response.data.summonerLevel,
+            accountId: response.data.accountId
+          },
+          () => {
+            console.log(this.state.accountId);
+          }
+        );
+        // this.setState({ name, summonerLevel, accountId });
+        // console.log(this.state.accountId);
+      });
 
-        this.setState({
-          name: response.data.name,
-          summonerLevel: response.data.summonerLevel,
-          accountId: response.data.accountId
-        });
+    axios
+      .get(
+        `https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}?api_key=${api_key}`
+      )
+      .then(response => {
+        console.log(response.data.matches);
+      })
+      .catch(err => {
+        console.log(err);
       });
   }
 
